715c60135ac2a44b9e5a298e2c09bbb14da5b0df
Marouen Bousnina <marouen.it.technology@gmail.com>
2020-04-09 09:51:33 +0200

    extract JSON data functions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { accounts, users, writeJSON } = require('./data');
 
 const app = express();
 
@@ -10,17 +11,6 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
-const accountData = fs.readFileSync(
-    path.join(__dirname, 'json', 'accounts.json'),
-    { encoding: "UTF8" }
-);
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(
-    path.join(__dirname, 'json', 'users.json'),
-    { encoding: "UTF8" }
-);
-const users = JSON.parse(userData);
 app.get('/', (req, res) => {
     res.render('index', { title: "Account Summary", accounts: accounts });
 });
@@ -49,13 +39,7 @@ app.post('/transfer', (req, res) => {
     const { from, to, amount } = req.body;
     accounts[from].balance -= parseInt(amount);
     accounts[to].balance += parseInt(amount);
-    const accountsJSON = JSON.stringify(accounts);
-
-    fs.writeFileSync(
-        path.join(__dirname, 'json/accounts.json'),
-        accountsJSON,
-        'utf8',
-    );
+    writeJSON();
     res.render('transfer', { message: "Transfer Completed" });
 
 });
@@ -69,15 +53,7 @@ app.post('/payment', (req, res) => {
 
     accounts.credit.balance -= parseInt(amount);
     accounts.credit.available += parseInt(amount);
-
-    const accountsJSON = JSON.stringify(accounts);
-
-    fs.writeFileSync(
-        path.join(__dirname, 'json/accounts.json'),
-        accountsJSON,
-        'utf8',
-    );
-
+    writeJSON();
     res.render('payment', { message: "Payment Successful", account: accounts.credit });
 
 });
